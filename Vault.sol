9a824a61c326b0f64d12e199caa7522d655e78b5
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-11-22 08:31:01 -0800

    Update Vault.sol

--- Vault.sol
+++ Vault.sol
@@ -1,1 +1,12 @@
+// SPDX- License-Identifier: MIT
 
+pragma solidity ^0.8.20;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC4626.sol";
+
+
+contract Vault is ERC4626{
+    constructor(IERC20 _asset) ERC4626(_asset) ERC20 ("Vault Jodge Token","VJT"){
+        
+    }
+}
